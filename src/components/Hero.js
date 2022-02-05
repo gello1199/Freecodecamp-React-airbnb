4c562add44c7f319d0be9edce29f6ab4386891ae
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-05 11:16:29 -0500

    Add Navbar component and start hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div>
+            <img className="hero--photo" src="https://media.istockphoto.com/photos/collage-of-printed-travel-images-picture-id527115069?b=1&k=20&m=527115069&s=170667a&w=0&h=S4FIBFawCGVZQs59ATSzY9DnJi29DGX7T8DVqzQJD_Y=" alt="travel img" />
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img className="navbar--logo" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c513.png" alt="airbnb logo" />
+        </nav>
+    )
+}
+
+export default Navbar;

2c07604c5a1aab7f24292c380ea1f8c30536a575
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-05 11:48:22 -0500

    Add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img className="card--img" src="https://images.unsplash.com/photo-1548182629-4c64b91b789d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="card img" />
+        </div>
+    );
+}
+
+export default Card;

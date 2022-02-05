8590373de31f888e6d1bc68034b2031a453a82f5
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-05 11:35:26 -0500

    Add CSS to Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <div>
+        <section className="hero">
             <img className="hero--photo" src="https://media.istockphoto.com/photos/collage-of-printed-travel-images-picture-id527115069?b=1&k=20&m=527115069&s=170667a&w=0&h=S4FIBFawCGVZQs59ATSzY9DnJi29DGX7T8DVqzQJD_Y=" alt="travel img" />
-        </div>
+
+            <h1 className="hero--title">Online Experiences</h1>
+
+            <p className="hero--p">Join unique interactive  activities led by one-of-a-kind hosts - all without leaving home.</p>
+
+        </section>
     )
 }
+
+export default Hero;

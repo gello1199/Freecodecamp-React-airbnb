d32bf9af7c5761f4e69afa9789c162edeac55a75
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-10 20:55:01 -0500

    Map over data file, return new cards

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import data from './data';
 
 const App = () => {
+
+    const cards = data.map(item => {
+       return (
+        <Card 
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+    />
+       )
+    })
+
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card 
-                img="colin-lloyd.png"
-                rating="5.0"
-                reviewCount={6}
-                location="USA"
-                title="Life Lessons with Olympic Athlete"
-                price={136}
-            />
+          {cards}
         </div>
     );
 }

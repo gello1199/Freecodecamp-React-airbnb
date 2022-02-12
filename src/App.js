2ce85a75b33116a9b3356aaba8bb529063fa89b2
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-11 21:07:05 -0500

    Changed item prop to use spread operator

--- src/App.js
+++ src/App.js
@@ -10,13 +10,9 @@ const App = () => {
        return (
         <Card 
             key={item.id}
-            img={item.coverImg}
-            rating={item.stats.rating}
-            reviewCount={item.stats.reviewCount}
-            location={item.location}
-            title={item.title}
-            price={item.price}
-            openSpots={item.openSpots}
+            {...item}
+            //same as below
+            // item={item}
     />
        )
     })
@@ -24,7 +20,7 @@ const App = () => {
     return (
         <div>
             <Navbar />
-            {/* <Hero /> */}
+            <Hero />
             <section className="cards-list">
                 {cards}
             </section>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Card = (props) => {
-
     let badgeText
     if(props.openSpots === 0) {
         badgeText = "SOLD OUT"
@@ -12,12 +11,12 @@ const Card = (props) => {
     return (
         <div className="card">
         {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img className="card--img" src={`../images/${props.img}`} alt="card img" />
+            <img className="card--img" src={`../images/${props.coverImg}`} alt="card img" />
 
             <div className="card--stats">
                 <img className="card--star" src="https://clipart.world/wp-content/uploads/2020/09/Orange-star-clipart-transparent.png" alt="star img" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
             </div>
             <p className="card--title">{props.title}</p>

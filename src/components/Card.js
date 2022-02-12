e6783491e649ed4648541fc881f784ea160415c0
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-11 20:33:49 -0500

    Added conditional to display sold out or online tag

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const Card = (props) => {
+
+    let badgeText
+    if(props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
-        {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+        {badgeText && <div className="card--badge">{badgeText}</div>}
             <img className="card--img" src={`../images/${props.img}`} alt="card img" />
 
             <div className="card--stats">
